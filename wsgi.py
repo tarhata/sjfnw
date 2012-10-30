9d706e10cea19fc2eedeeaea253269ffe5399745
Aisa <aisapatino@gmail.com>
2012-10-29 17:24:16 -0700

    log exceptions

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,15 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
+import django.core.signals
 from django.core.wsgi import get_wsgi_application
+import django.dispatch.dispatcher
+
+def log_exception(*args, **kwds):
+    logging.exception('Exception in request:')
+
+# Log errors.
+#django.dispatch.dispatcher.connect(
+    #log_exception, django.core.signals.got_request_exception)
 
 application = get_wsgi_application()
