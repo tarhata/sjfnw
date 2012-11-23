a7eb001666f33f914a58b4fe5210b1504147dccf
Aisa <aisapatino@gmail.com>
2012-11-22 23:39:09 -0800

    removing django folder; gae supports 1.4 now

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os
 
+WSGI_APPLICATION = 'wsgi.application'
+
 SECRET_KEY = '*r-$b*8hglm+959&7x043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or os.getenv('SETTINGS_MODE') == 'prod'):
