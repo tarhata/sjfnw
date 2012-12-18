5c3dc65fbf3b639f790cc53ba7980f51aa9fbfee
Aisa <aisapatino@gmail.com>
2012-12-18 14:18:36 -0800

    script to set/clear next_step
    cause i was missing a clear in donestep

--- fund/management/commands/update_db.py
+++ fund/management/commands/update_db.py
@@ -3,7 +3,18 @@ from fund.models import Donor, Step
 
 class Command(BaseCommand):
 
-  help = 'Not in use'
-
+  help = 'Fixes next step associations'
+  
   def handle(self, *args, **options):
-    self.stdout.write('This is empty.')
+    self.stdout.write('Beginning.\n')
+    for donor in Donor.objects.all():
+      self.stdout.write(str(donor))
+      steps = donor.step_set.filter(completed__isnull = True)
+      if steps:
+        donor.next_step = steps[0]
+        self.stdout.write(' - next_step set\n')
+      else:
+        donor.next_step = None
+        self.stdout.write(' - next_step set to None\n')
+      donor.save()
+    self.stdout.write('Script complete.')
