541d4c58fb22fdb893781fbc136a5954e46b23c0
Aisa <aisapatino@gmail.com>
2013-01-06 13:19:58 -0800

    logging on redirects

--- fund/decorators.py
+++ fund/decorators.py
@@ -1,6 +1,7 @@
 ﻿from functools import wraps
 from django.shortcuts import redirect
 from django.utils.decorators import available_attrs
+import logging
 
 def approved_membership(function=None):
   def decorator(view_func):
@@ -9,8 +10,10 @@ def approved_membership(function=None):
       if request.membership_status==3: #success, just do the view func
         return view_func(request, *args, **kwargs)
       elif request.membership_status==2: #membership(s) not approved
+        logging.info('Membership(s) not approved, redirecting to pending')
         return redirect('/fund/pending')
       elif request.membership_status==1: #no memberships
+        logging.info('No memberships, redirecting to projects page')
         return redirect('/fund/projects')
       else: #no member object
         return redirect('/fund/not-member')
