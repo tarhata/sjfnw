8b0ab0c37f011a87c61ed04358ae35689c9cf08b
Aisa <aisapatino@gmail.com>
2013-01-05 18:46:14 -0800

    trying to make tests
    #106

--- fund/tests.py
+++ fund/tests.py
@@ -1,17 +1,40 @@
 from django.utils import unittest
-from fund import models
+from django.test.client import Client
 
-class SimpleTest(unittest.TestCase):
+from fund import models
+from django.contrib.auth.models import User
+
+class StepCompleteTest(unittest.TestCase):
+    fixtures = ['test_fund']
     
     def setUp(self):
-      pass
+      self.client = Client()
+      user = User.objects.create_user('testacct@gmail.com', 'testacct@gmail.com', 'testy')
+      self.client.login(username = 'testacct@gmail.com', password = 'testy')
+      
 
+    def test_valid_asked(self):
+      form_data = {'asked': 'on',
+          'response': 2,
+          'pledged_amount': '',
+          'last_name': '',
+          'notes': '',
+          'next_step': '',
+          'next_step_date': ''}
+      
+      response = self.client.post('/fund/1/1/', form_data)
+      
+      self.assertEqual(response.status_code, 200)
+      
+    
+    
 """
 test ideas:
-
-fixtures for fake db
-
-registration
+  registration
+  step complete
+  fresh page w/o contacts
+  pre & post estimate
+  notifications
+  forms in general
   attempt regis w/repeat & non
-  
-  
+"""
