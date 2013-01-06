bfcfc22e95d29c147523a8d48a8ee3b5c334958f
Aisa <aisapatino@gmail.com>
2013-01-06 13:19:26 -0800

    added paths
    works! #106

--- fund/tests.py
+++ fund/tests.py
@@ -1,13 +1,18 @@
-from django.utils import unittest
+from django.test import TestCase
 from django.test.client import Client
-
 from fund import models
 from django.contrib.auth.models import User
-
-class StepCompleteTest(unittest.TestCase):
-    fixtures = ['test_fund']
+import logging
+import sys
+  
+class StepCompleteTest(TestCase):
+    fixtures = ['fund/fixtures/test_fund.json',]
     
     def setUp(self):
+      #add libs to the path that dev_appserver normally takes care of
+      sys.path.append('C:\Program Files (x86)\Google\google_appengine\lib\yaml\lib')
+      sys.path.append('C:\Program Files (x86)\Google\google_appengine\lib\webob_1_1_1')
+      
       self.client = Client()
       user = User.objects.create_user('testacct@gmail.com', 'testacct@gmail.com', 'testy')
       self.client.login(username = 'testacct@gmail.com', password = 'testy')
@@ -23,7 +28,7 @@ class StepCompleteTest(unittest.TestCase):
           'next_step_date': ''}
       
       response = self.client.post('/fund/1/1/', form_data)
-      
+      print(response.templates)
       self.assertEqual(response.status_code, 200)
       
     
