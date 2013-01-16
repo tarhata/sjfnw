7c85137c3be9fea442b8ede16fbfffde4f3409a8
Aisa <aisapatino@gmail.com>
2013-01-15 20:49:46 -0800

    starting to list tests
    #116

--- grants/tests.py
+++ grants/tests.py
@@ -1,16 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from grants import models
+   
+""" TESTS TO DO
+      discard draft button
+      submitted apps sorting
+      password reset on live
+      file upload/serving? """
