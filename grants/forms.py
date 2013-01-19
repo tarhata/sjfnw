02f62fb44948079800b48099fa45023ee851a66d
Aisa <aisapatino@gmail.com>
2013-01-18 16:34:05 -0800

    enter pw twice
    #133

--- grants/forms.py
+++ grants/forms.py
@@ -2,10 +2,21 @@ from django import forms
 import models, datetime
 
 class LoginForm(forms.Form):
-	email = forms.EmailField()
-	password = forms.CharField(widget=forms.PasswordInput())
+  email = forms.EmailField()
+  password = forms.CharField(widget=forms.PasswordInput())
 
 class RegisterForm(forms.Form):
-	email = forms.EmailField()
-	password = forms.CharField(widget=forms.PasswordInput())
-	organization = forms.CharField()
+  email = forms.EmailField()
+  password = forms.CharField(widget=forms.PasswordInput())
+  passwordtwo = forms.CharField(widget=forms.PasswordInput(), label="Re-enter password")
+  organization = forms.CharField()
+  
+  def clean(self): #make sure passwords match
+    cleaned_data = super(RegisterForm, self).clean()
+    password = cleaned_data.get("password")
+    passwordtwo = cleaned_data.get("passwordtwo")
+    if password and passwordtwo and password != passwordtwo:
+      self._errors["password"] = self.error_class(["Passwords did not match."])
+      del cleaned_data["password"]
+      del cleaned_data["passwordtwo"]
+    return cleaned_data  
