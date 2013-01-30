7333e301008171aa7817bf648007c8fe7c78e43f
Aisa <aisapatino@gmail.com>
2013-01-30 15:06:57 -0800

    func for getting file from filefield
    shared between draft and submitted file views #203

---
+++ grants/utils.py
@@ -0,0 +1,47 @@
+﻿from django.conf import settings
+from django.http import HttpResponse, Http404
+from django.utils import timezone
+from google.appengine.ext import blobstore
+import datetime, logging
+
+def FindBlob(application, file_type):
+  """Return file from the Blobstore.  Args:
+  application = GrantApplication or DraftGrantApplication object
+  file_type: str indicating which file field """
+  
+  #find the file
+  if file_type == 'budget':
+    file_field = application.budget
+  elif file_type == 'demographics':
+    file_field = application.demographics
+  elif file_type == 'funding':
+    file_field = application.funding_sources
+  else:
+    logging.warning('Unknown file type ' + file_type)
+    return Http404
+  
+  #filefield stores key that gets us the blobinfo
+  blobinfo_key = str(file_field).split('/', 1)[0]
+  blobinfo = blobstore.BlobReader(blobinfo_key).readlines()
+  #look through the info for the creation time of the blob
+  blobinfo_dict =  dict([l.split(': ', 1) for l in blobinfo if l.strip()])
+  creation_time = blobinfo_dict['X-AppEngine-Upload-Creation'].strip()
+  
+  if not settings.DEBUG: #convert to datetime for live
+    creation_time = datetime.datetime.strptime(creation_time, '%Y-%m-%d %H:%M:%S.%f')
+    creation_time = timezone.make_aware(creation_time, timezone.get_current_timezone())
+  
+  logging.info('Looking for: ' + str(creation_time))
+  
+  #find blob that matches the creation time
+  for b in  blobstore.BlobInfo.all():    
+    c = b.creation
+    if settings.DEBUG: #local - just compare strings
+      if str(timezone.localtime(c)) == creation_time:
+        return HttpResponse(blobstore.BlobReader(b).read(), content_type=b.content_type)
+    else:
+      c = timezone.make_aware(c, timezone.utc)
+      if timezone.localtime(c) == creation_time:
+        return HttpResponse(blobstore.BlobReader(b).read(), content_type=b.content_type)
+  logging.warning('No blob matching the creation time')
+  return Http404
