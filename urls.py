1804207fdd5534e1263891f70174c005ef2d06e2
Aisa <aisapatino@gmail.com>
2013-01-30 15:07:29 -0800

    better file view urls
    "download" was misleading

--- urls.py
+++ urls.py
@@ -52,7 +52,8 @@ urlpatterns = patterns('',
 ## GRANTS - APPLICATION VIEWS ##
 
     (r'^grants/view/(?P<app_id>\d+)/?$', 'grants.views.ViewApplication'),
-    (r'^grants/download/(?P<app_id>\d+)/(?P<file_type>.*)/?$', 'grants.views.ViewFile'),
+    (r'^grants/view-file/(?P<app_id>\d+)/(?P<file_type>.*)/?$', 'grants.views.ViewFile'),
+    (r'^grants/draft-file/(?P<draft_id>\d+)/(?P<file_type>.*)/?$', 'grants.views.ViewDraftFile'),
     
 ## GRANTS - REPORTING ##
   
