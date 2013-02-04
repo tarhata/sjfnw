fd0b0b4e7cb2514ce6d4937568ac6700aef198b2
Aisa <aisapatino@gmail.com>
2013-02-04 13:35:44 -0800

    mild tweaks to file serving, handle letter
    mostly logging #214

--- grants/utils.py
+++ grants/utils.py
@@ -5,8 +5,9 @@ from google.appengine.ext import blobstore
 import datetime, logging
 
 def FindBlob(application, file_type):
-  """Return file from the Blobstore.  Args:
-  application = GrantApplication or DraftGrantApplication object
+  """Return file from the Blobstore.
+
+  application: GrantApplication or DraftGrantApplication
   file_type: str indicating which file field """
   
   #find the file
@@ -16,6 +17,8 @@ def FindBlob(application, file_type):
     file_field = application.demographics
   elif file_type == 'funding':
     file_field = application.funding_sources
+  elif file_type == 'fiscal_letter':
+    file_field = application.fiscal_letter
   else:
     logging.warning('Unknown file type ' + file_type)
     return Http404
@@ -24,17 +27,24 @@ def FindBlob(application, file_type):
   blobinfo_key = str(file_field).split('/', 1)[0]
   logging.info('Info key: ' + blobinfo_key)
   binfo = blobstore.BlobInfo.get(blobinfo_key)
-  logging.info('Blob creation: ' + str(binfo.creation))
-  logging.info('Blob content type: ' + str(binfo.content_type))
-  return HttpResponse(blobstore.BlobReader(binfo).read(), content_type=binfo.content_type)
+  #all binfo properties refer to the blobinfo itself, not the blob  
+  reader = blobstore.BlobReader(binfo) #reads the info file contents which refer to the actual blob
   
-  """
-  for l in blobinfo:
-    print(l)
+  """ example contents:
+    Content-Type: application/pdf
+    MIME-Version: 1.0
+    Content-Length: 7916790
+    Content-MD5: OGRkMTkzOGYxZWQ3NjhlMWY4OWNhYjVlMjQ4YWQ1ODc=
+    content-type: application/pdf
+    content-disposition: form-data; name="fiscal_letter"; filename="persuasive technology.pdf"
+    X-AppEngine-Upload-Creation: 2013-02-04 20:58:26.170000 """
+    
   #look through the info for the creation time of the blob
-  blobinfo_dict =  dict([l.split(': ', 1) for l in blobinfo if l.strip()])
+  blobinfo_dict =  dict([l.split(': ', 1) for l in reader if l.strip()])
   creation_time = blobinfo_dict['X-AppEngine-Upload-Creation'].strip()
-  #Content-Disposition: form-data; name=file1; filename="ODO.jpg"
+  logging.info('Blob creation: ' + creation_time)
+  content_disp = blobinfo_dict['content-disposition'].strip()
+  logging.info('Blob content-disp: ' + content_disp)
   
   if not settings.DEBUG: #convert to datetime for live
     creation_time = datetime.datetime.strptime(creation_time, '%Y-%m-%d %H:%M:%S.%f')
@@ -53,4 +63,4 @@ def FindBlob(application, file_type):
       if timezone.localtime(c) == creation_time:
         return HttpResponse(blobstore.BlobReader(b).read(), content_type=b.content_type)
   logging.warning('No blob matching the creation time')
-  return Http404 """
+  return Http404
