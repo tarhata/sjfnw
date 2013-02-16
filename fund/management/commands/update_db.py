d68a8071df7b5150c3d422ea0755a1b9552a9743
Aisa <aisapatino@gmail.com>
2013-02-16 08:32:10 -0800

    output

--- fund/management/commands/update_db.py
+++ fund/management/commands/update_db.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
     self.stdout.write('Beginning.\n')
     for donor in Donor.objects.all():
-      self.stdout.write(str(donor))
+      self.stdout.write(str(donor) + ' ' + str(donor.next_step is not None))
       steps = donor.step_set.filter(completed__isnull = True)
       if steps:
         donor.next_step = steps[0]
         self.stdout.write(' - next_step set\n')
       else:
         donor.next_step = None
-        self.stdout.write(' - next_step set to None\n')
+        self.stdout.write(' - next_step None\n')
       donor.save()
     self.stdout.write('Script complete.')
