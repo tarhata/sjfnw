699f9dbf3eecab2f2acdd22022e1916c5aea4b19
Aisa <aisapatino@gmail.com>
2013-02-19 11:03:05 -0800

    code cleanup
    lines, indents

--- grants/utils.py
+++ grants/utils.py
@@ -114,17 +114,17 @@ def DeleteEmptyFiles(request): #/tools/delete-empty
 
 #User username length patch
 def patch_user_model(model):
+  
   field = model._meta.get_field("username")
-
   field.max_length = 100
   field.help_text = "Required, 100 characters or fewer. Only letters, numbers, and @, ., +, -, or _ characters."
 
-  # patch model field validator because validator doesn't change if we change
-  # max_length
+  # patch model field validator because validator doesn't change if we change max_length
   for v in field.validators:
-      if isinstance(v, MaxLengthValidator):
-          v.limit_value = 100
+    if isinstance(v, MaxLengthValidator):
+      v.limit_value = 100
   
+  # patch admin site forms
   from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 
   UserChangeForm.base_fields['username'].max_length = 100
@@ -143,4 +143,4 @@ def patch_user_model(model):
   AuthenticationForm.base_fields['username'].help_text = UserChangeForm.base_fields['username'].help_text.replace('30', '100')
 
 if User._meta.get_field("username").max_length != 100:
-    patch_user_model(User)
+  patch_user_model(User)
