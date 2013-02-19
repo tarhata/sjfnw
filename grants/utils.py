fdfd43b4b7dab1833b6d7baa2aade448eb9015eb
Aisa <aisapatino@gmail.com>
2013-02-19 10:59:32 -0800

    hack the admin login form too
    #279

--- grants/utils.py
+++ grants/utils.py
@@ -125,17 +125,22 @@ def patch_user_model(model):
       if isinstance(v, MaxLengthValidator):
           v.limit_value = 100
   
-  from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+  from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 
   UserChangeForm.base_fields['username'].max_length = 100
   UserChangeForm.base_fields['username'].widget.attrs['maxlength'] = 100
   UserChangeForm.base_fields['username'].validators[0].limit_value = 100
-  UserChangeForm.base_fields['username'].help_text = UserChangeForm.base_fields['username'].help_text.replace('30', str(100))
+  UserChangeForm.base_fields['username'].help_text = UserChangeForm.base_fields['username'].help_text.replace('30', '100')
 
   UserCreationForm.base_fields['username'].max_length = 100
   UserCreationForm.base_fields['username'].widget.attrs['maxlength'] = 100
   UserCreationForm.base_fields['username'].validators[0].limit_value = 100
-  UserCreationForm.base_fields['username'].help_text = UserChangeForm.base_fields['username'].help_text.replace('30', str(100))
+  UserCreationForm.base_fields['username'].help_text = UserChangeForm.base_fields['username'].help_text.replace('30', '100')
+  
+  AuthenticationForm.base_fields['username'].max_length = 100
+  AuthenticationForm.base_fields['username'].widget.attrs['maxlength'] = 100
+  AuthenticationForm.base_fields['username'].validators[0].limit_value = 100
+  AuthenticationForm.base_fields['username'].help_text = UserChangeForm.base_fields['username'].help_text.replace('30', '100')
 
 if User._meta.get_field("username").max_length != 100:
     patch_user_model(User)
