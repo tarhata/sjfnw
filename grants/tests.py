bb5f7310860987f180ee206648bd6032f02d6318
Aisa <aisapatino@gmail.com>
2013-02-19 22:33:31 -0800

    got file upload working in test!
    #283

--- grants/tests.py
+++ grants/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.test.utils import override_settings
 from django.utils import timezone
 from django.utils.html import strip_tags
 from grants.models import GrantApplication, DraftGrantApplication, Organization, GrantCycle
@@ -10,82 +11,163 @@ def setPaths():
   sys.path.append('C:\Program Files (x86)\Google\google_appengine\lib\yaml\lib')
   sys.path.append('C:\Program Files (x86)\Google\google_appengine\lib\webob_1_1_1')
 
-def logInTesty(self):
+def setCycleDates():
+  #open, open, closed, upcoming, open
+  now = timezone.now()
+  ten_days = datetime.timedelta(days=10)
+  twenty_days = datetime.timedelta(days=20)
+  
+  cycle = GrantCycle.objects.get(pk=1)
+  cycle.open = now - ten_days
+  cycle.close = now + ten_days
+  cycle.save()
+  cycle = GrantCycle.objects.get(pk=2)
+  cycle.open = now - ten_days
+  cycle.close = now + ten_days
+  cycle.save()
+  cycle = GrantCycle.objects.get(pk=3)
+  cycle.open = now - twenty_days
+  cycle.close = now - ten_days
+  cycle.save()
+  cycle = GrantCycle.objects.get(pk=4)
+  cycle.open = now + ten_days
+  cycle.close = now + twenty_days
+  cycle.save()
+  cycle = GrantCycle.objects.get(pk=5)
+  cycle.open = now - twenty_days
+  cycle.close = now + ten_days
+  cycle.save()
+
+def logInTesty(self): #'OfficeMax Foundation' pk 2. Submitted 1, draft 1
   self.user = User.objects.create_user('testacct@gmail.com', 'testacct@gmail.com', 'testy')
   self.client.login(username = 'testacct@gmail.com', password = 'testy')
 
-class ViewApplicationTests(TestCase):
+def logInNewbie(self): #'Fresh New Org' pk 1
+  user = User.objects.create_user('newacct@gmail.com', 'newacct@gmail.com', 'noob')
+  self.client.login(username = 'newacct@gmail.com', password = 'noob')
+
+class ApplyTests(TestCase):
   
-  fixtures = ['grants/fixtures/grantapps.json',] 
+  """Submitting the application form"""
+  
+  fixtures = ['grants/fixtures/test_grants.json',] 
   
   def setUp(self):
+    setPaths()
+    logInNewbie(self)
+    setCycleDates()
+    self.org = Organization.objects.get(pk = 1)
+  
+  @override_settings(DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage')
+  @override_settings(MEDIA_ROOT = 'media/')
+  @override_settings(FILE_UPLOAD_HANDLERS = ('django.core.files.uploadhandler.MemoryFileUploadHandler',))
+  def test_post_valid_app(self):
+    #check 0 submitted apps
     
-    setPaths()      
-    logInTesty(self)
+    #fake form submit using same txt for all 3 files
+    form_data = {u'website': [u'asdfsdaf'],
+            u'mission': [u'A kmission statement of some importance!'],
+            u'founded': [u'351'],
+            u'fiscal_telephone': [u''],
+            u'email_address': [u'as@gmail.com'],
+            u'city': [u'sdaf'],
+            u'amount_requested': [u'100000'],
+            u'zip': [u'654'],
+            u'start_year': [u'sdfsadfdsf'],
+            u'project_budget': [u''],
+            u'grant_cycle': [u'2'],
+            u'support_type': [u'General support'],
+            u'state': [u'OR'],
+            u'fiscal_org': [u''],
+            u'status': [u'501c3'],u'narrative1': [u'adasrdsadssdfsdfsdfsdfdfsdfsdfdfsdfsdf\r\r\ndfsdfsdfdfsdfsdfdfsdfsdfdfsdfsdfsdfsdfdfsdfsdfdfsdfsdfdfsdfsdfdfdfsdfdfsdfsdfdfsdfsdfdfdfsdfsdfdfsdfsdfdfsdfsdf'],
+            u'narrative2': [u'sdfsdfsdfitizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
+            u'narrative3': [u'sdfasfsdfsdfdsfdsfsdffsdfitizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are theroot causes of thesefdsfsdfsdfsdfsdfsdfsdfsdfds'],
+            u'narrative4': [u'itizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of theseitizesgroups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
+            u'narrative5': [u'itizes groups that understand and address the underlying, orroot causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of theseitizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
+            u'narrative6': [u'itizes groups that klady adskhaskjdfhasdkjgfsa faksjdhfsakdfh ogether to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
+            u'fax_number': [u'321'],
+            u'budget_last': [u'256161'],
+            u'address': [u'asdfsdf'],
+            u'fiscal_email': [u''],
+            u'grant_period': [u'sdfgsdfdsaf'],
+            u'previous_grants': [u'dsfsadfsfdsa dsg gdfg sadfdsg fd g'],
+            u'grant_request': [u'A grant rewuireasjdflsdfasdg'],
+            u'fiscal_person': [u''],
+            u'project_title': [u''],
+            u'budget_current': [u'62561'],
+            u'fiscal_address': [u''],
+            u'telephone_number': [u'325'],
+            u'organization': [u'1'],
+            u'contact_person': [u'asdfsadfasdfasdf'],
+            u'contact_person_title': [u'Dr Mr'],
+            u'ein': [u'654'],
+            u"collab_ref1_name": "Audrey", 
+            u"collab_ref1_phone": [u"206568756"], 
+            u"collab_ref1_org": [u"Organzzzz"], 
+            u"collab_ref2_name": [u"Hep"], 
+            u"collab_ref2_phone": [u""], 
+            u"collab_ref2_org": [u"RFLAEKJRKH"], 
+            u"collab_ref2_email": [u"asdasdsad@gface.com"], 
+            u"collab_ref1_email": [u"adsasd@gjadslf.com"],
+            u'screening_status': [u'10'],
+    }
+    budget =  open('grants/fixtures/test_grants_guide.txt')
+    form_data['budget'] = budget
+    funding_sources =  open('static/grant_app/funding.doc')
+    form_data['funding_sources'] = funding_sources
+    demographics = open('static/css/admin.css')
+    form_data['demographics'] = demographics
     
-    self.cycle = GrantCycle(title= 'Open cycle', open = timezone.now()-datetime.timedelta(days=2), close = timezone.now()+datetime.timedelta(days=2))
-    self.cycle.save()  
-    
-  def test_fields(self):
-    """ Application view vs. form """
+    #POST
+    response = self.client.post('/apply/1/', form_data)
+    budget.close()
+    funding_sources.close()
+    demographics.close()
+    print(response.context)
+    print(", ".join(t.name for t in response.templates))
+    #self.assertTemplateUsed(response, 'grants/submitted.html')
+    self.assertEqual(response.status_code, 302)
 
-    response = self.client.get('/apply/' + str(self.cycle.pk))
-    self.assertEqual(response.status_code, 200)
-    self.assertTemplateUsed(response, 'grants/org_app.html')
-    form_labels = []
-    #print('Form labels:\n')
-    for label in re.findall('<span class="label">.+</span>', response.content):
-      #print(strip_tags(label))
-      form_labels.append(strip_tags(label))
+class PageLoadTests(TestCase):
     
-    response = self.client.get('/grants/view/1')
-    self.assertEqual(response.status_code, 200)
-    self.assertTemplateUsed(response, 'grants/view_app.html')
-    view_labels = []
-    #print('View labels:\n')
-    for label in re.findall('<span class="label">.+</span>', response.content):
-      #print(strip_tags(label))
-      view_labels.append(strip_tags(label))
-    print('In form but not view:\n')
-    print('\n'.join(set(form_labels).difference(set(view_labels))))
-    """
-    do the same for the view app and compare! """
+  def test_load_first_app(self):
+    pass
+    #expect empty form
+  
+  def test_load_second_app(self):
+    pass
+    #expect profile fields
+  
+  def test_load_home_page(self):
+    pass
+    """ submitted apps sorting
+        display of submitted, drafts, past-due drafts
+        display/sorting of cycles"""
+
+class BlockedLoadTests(TestCase):
+  """apply to non-existent cycle
+    apply to closed cycle
+    apply to upcoming cycle
+    apply to one you've applied to"""
+        
+class DraftTests(TestCase):
+   #can't test autosave here..?
+  def test_discard(self):
+    pass
+    #discard a draft
     
 """ TESTS TO DO
       
-      try to access pages without being registered
-      loading app page for the first time
-      brand new org
-      org with profile info saved
-      file upload/serving?
-      
-      test functions:
-        discard a draft
-        basic page loads
-      
-      org home page:
-        submitted apps sorting
-        display of submitted, drafts, past-due drafts
-        display/sorting of cycles
-      
-      test error cases:
-        apply to non-existent cycle
-        apply to closed cycle
-        apply to upcoming cycle
-        apply to one you've applied to
-        
+    try to access pages without being registered
+    file upload/serving?
+   
     FIXTURE NEEDS
-      grant cycles: 
-        past, upcoming, several open
-        [might be worth creating in script so we don't have to worry about dates]
-      
       orgs:
         brand new
         has saved profile
         has applied to 1+ open apps
         has saved draft that's still open
         has saved draft that's past-due
-
  """
 
 """
@@ -107,40 +189,3 @@ class GrantApplicationTests(TestCase)
     self.assertTemplateUsed(response, 'tests/solution_detail.html')
     self.assertContains(response, os.path.basename(filename))
   """
-
-"""    
-TEPOST_DICTS = [
-  valid =  {u'website': [u'asdfsdaf'],
-            u'mission': [u'A kmission statement of some importance!'],
-            u'founded': [u'351'],
-            u'fiscal_telephone': [u''],
-            u'email_address': [u'as@gmail.com'],
-            u'city': [u'sdaf'],
-            u'amount_requested': [u'100000'],
-            u'zip': [u'654'],
-            u'start_year': [u'sdfsadfdsf'],
-            u'project_budget': [u''],
-            u'grant_cycle': [u'2'],
-            u'support_type': [u'General support'],
-            u'state': [u'OR'],
-            u'fiscal_org': [u''],
-            u'status': [u'501c3'],u'narrative1': [u'adasrdsadssdfsdfsdfsdfdfsdfsdfdfsdfsdf\r\r\ndfsdfsdfdfsdfsdfdfsdfsdfdfsdfsdfsdfsdfdfsdfsdfdfsdfsdfdfsdfsdfdfdfsdfdfsdfsdfdfsdfsdfdfdfsdfsdfdfsdfsdfdfsdfsdf'],
-            u'narrative2': [u'sdfsdfsdfitizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
-            u'narrative3': [u'sdfasfsdfsdfdsfdsfsdffsdfitizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are theroot causes of thesefdsfsdfsdfsdfsdfsdfsdfsdfds'],
-            u'narrative4': [u'itizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of theseitizesgroups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
-            u'narrative5': [u'itizes groups that understand and address the underlying, orroot causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of theseitizes groups that understand and address the underlying, or root causes of the issues, and that bring people together to build collective power.\r\r\nWhat problems, needs or issues does your work address?\r\r\nWhat are the root causes of these'],
-            u'fax_number': [u'321'],
-            u'budget_last': [u'256161'],
-            u'address': [u'asdfsdf'],
-            u'fiscal_email': [u''],
-            u'grant_period': [u'sdfgsdfdsaf'],
-            u'previous_grants': [u'dsfsadfsfdsa dsg gdfg sadfdsg fd g'],
-            u'grant_request': [u'A grant rewuireasjdflsdfasdg'],
-            u'fiscal_person': [u''],
-            u'project_title': [u''],
-            u'budget_current': [u'62561'],
-            u'fiscal_address': [u''],
-            u'telephone_number': [u'325'],
-            u'organization': [u'1'],
-            u'contact_person': [u'asdfsadfasdfasdf'],
-            u'ein': [u'654']} """
