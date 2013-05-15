b0f1bcf592b63be516dd2515ffc0e1264e441417
Aisa <aisapatino@gmail.com>
2013-05-15 14:13:45 -0700

    move the truncate to the storage handler, when it returns the string to be stored. (will still display full file name to user initially bc it's using the blobinfo)

--- sjfnw/grants/views.py
+++ sjfnw/grants/views.py
@@ -293,21 +293,18 @@ def AddFile(request, draft_id):
   draft = get_object_or_404(models.DraftGrantApplication, pk=draft_id)
   logging.info(unicode(draft.organization))
   logging.info([request.body]) #don't remove this without fixing storage to not access body
-  file_name = False
+  blob_file = False
   for key in request.FILES:
-    file_name = unicode(request.FILES[key])
-    logging.info(file_name)
-    if file_name:
+    blob_file = request.FILES[key]
+    logging.info(blob_file)
+    if blob_file:
       if hasattr(draft, key):
         # delete previous file
         old = getattr(draft, key)
         if old:
           deferred.defer(utils.DeleteBlob, old)
         # set new file
-        if len(file_name) > 65:
-          #shorten it so extension fits in FileField
-          file_name = file_name.split(".")[0][:60].rstrip() + u'.' + file_name.split(".")[1]
-        setattr(draft, key, file_name)
+        setattr(draft, key, blob_file)
         field_name = key
         break
       else:
@@ -315,12 +312,11 @@ def AddFile(request, draft_id):
   draft.modified = timezone.now()
   draft.save()
   
-  logging.info(file_name)
-  if not (file_name and field_name):
+  if not (blob_file and field_name):
     return HttpResponse("ERRORRRRRR")
   
   file_urls = GetFileURLs(draft)
-  content = field_name + u'~~<a href="' + file_urls[field_name] + u'" target="_blank" title="' + file_name + u'">' + file_name + u'</a> [<a onclick="removeFile(\'' + field_name + u'\');">remove</a>]'
+  content = field_name + u'~~<a href="' + file_urls[field_name] + u'" target="_blank" title="' + unicode(blob_file) + u'">' + unicode(blob_file) + u'</a> [<a onclick="removeFile(\'' + field_name + u'\');">remove</a>]'
   logging.info(u"AddFile returning: " + content)
   return HttpResponse(content)
 
