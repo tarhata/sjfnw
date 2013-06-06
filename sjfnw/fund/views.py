65b6a2197fe424732dc1c0f639dcfbffcd2afeaa
Aisa <aisapatino@gmail.com>
2013-06-06 13:09:02 -0700

    fix indent

--- sjfnw/fund/views.py
+++ sjfnw/fund/views.py
@@ -30,9 +30,9 @@ def get_block_content(membership, first=True):
      contents.append(models.Step.objects.select_related('donor').filter(donor__membership=membership, completed__isnull=True).order_by('date')[:2])
   contents.append(models.NewsItem.objects.filter(membership__giving_project=membership.giving_project).order_by('-date'))
 
-if membership.giving_project.pk == 14: # hack for PDX to view NGGP
-  contents.append(GrantApplication.objects.filter(giving_project_id=12, screening_status__gte=50).order_by('organization__name'))
-else:
+  if membership.giving_project.pk == 14: # hack for PDX to view NGGP
+    contents.append(GrantApplication.objects.filter(giving_project_id=12, screening_status__gte=50).order_by('organization__name'))
+  else:
     contents.append(GrantApplication.objects.filter(giving_project=membership.giving_project, screening_status__gte=50).order_by('organization__name'))
   #logging.info(contents)
   return contents
