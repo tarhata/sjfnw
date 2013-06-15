69f4a4237caa6379595935a2c2824c00d5d7799a
aisa <aisapatino@gmail.com>
2013-06-14 17:09:20 -0700

    had to use this again

--- sjfnw/fund/management/commands/update_stories.py
+++ sjfnw/fund/management/commands/update_stories.py
@@ -6,19 +6,20 @@ import datetime
 
 class Command(BaseCommand):
 
-  help = 'Runs update story for missed days 5-21 to 5-28'
+  help = 'Runs update story for missed days 6-13 to 6-14'
 
   def handle(self, *args, **options):
     self.stdout.write('Beginning.\n')
-    start = datetime.datetime.strptime('2013-05-21 03:04:01',
+    start = datetime.datetime.strptime('2013-06-13 03:04:01',
                                        '%Y-%m-%d %H:%M:%S')
     start = timezone.make_aware(start, timezone.get_current_timezone())
-    end = datetime.datetime.strptime('2013-05-28 05:06:01',
+    end = datetime.datetime.strptime('2013-06-14 05:06:01',
                                      '%Y-%m-%d %H:%M:%S')
     end = timezone.make_aware(end, timezone.get_current_timezone())
     # Get all memberships who completed a step within this range
     ships = (Step.objects.filter(completed__range=(start, end))
-                        .values_list('donor__membership', flat=True))
+                        .values_list('donor__membership', flat=True)
+                        .distinct())
 
     for ship in ships:
       self.stdout.write('creating stories for ' + unicode(ship) + '\n')
@@ -26,3 +27,4 @@ class Command(BaseCommand):
         UpdateStory(ship, start + datetime.timedelta(days=i))
 
     self.stdout.write('Script complete.\n')
+
