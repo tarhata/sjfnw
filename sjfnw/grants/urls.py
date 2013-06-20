20ada437cd7de0b77c0a545b686b99c9d8eaa243
aisa <aisapatino@gmail.com>
2013-06-19 23:39:59 -0700

    split urls into separate files/sets

---
+++ sjfnw/grants/urls.py
@@ -0,0 +1,47 @@
+from django.conf.urls import patterns, include
+from django.views.generic.simple import direct_to_template
+from sjfnw import constants
+
+apply_urls = patterns('',
+  (r'^nr', direct_to_template, {'template': 'grants/not_grantee.html'}),
+  (r'^submitted/?', direct_to_template, {'template': 'grants/submitted.html'}),
+)
+
+apply_urls += patterns('grants.views',
+
+  #login, logout, registration
+  (r'^login/?$', 'org_login'),
+  (r'^register/?$', 'org_register'),
+
+  #home page
+  (r'^$','org_home'),
+  (r'^(?P<draft_id>\d+)/DELETE/?$', 'DiscardDraft'),
+  (r'^copy/?$', 'CopyApp'),
+  (r'^support/?', 'org_support'),
+
+  #application
+  (r'^(?P<cycle_id>\d+)/?$','Apply'),
+  (r'^info/(?P<cycle_id>\d+)/?$','cycle_info'),
+
+  #application ajax
+  (r'^(?P<draft_id>\d+)/add-file/?$', 'AddFile'),
+  (r'^(?P<draft_id>\d+)/remove/(?P<file_field>.*)/?$', 'RemoveFile'),
+  (r'^(?P<cycle_id>\d+)/autosave/?$','AutoSaveApp'),
+)
+
+apply_urls += patterns('',
+  # password reset
+  (r'^reset/?$', 'django.contrib.auth.views.password_reset', {'template_name':'grants/reset.html', 'from_email':constants.GRANT_EMAIL, 'email_template_name':'grants/password_reset_email.html', 'post_reset_redirect':'/apply/reset-sent'}),
+  (r'^reset-sent/?$', 'django.contrib.auth.views.password_reset_done', {'template_name':'grants/password_reset_done.html'}),
+  (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/?$', 'django.contrib.auth.views.password_reset_confirm', {'template_name':'grants/password_reset_confirm.html', 'post_reset_redirect': '/apply/reset-complete'}, 'org-reset'),
+  (r'^reset-complete/?', 'django.contrib.auth.views.password_reset_complete', {'template_name':'grants/password_reset_complete.html'}),
+)
+
+grants_urls = patterns('grants.views',
+  #reading
+  (r'^grants/view/(?P<app_id>\d+)/?$', 'ReadApplication'),
+  (r'^grants/view-file/(?P<app_id>\d+)-(?P<file_type>.*)\.', 'ViewFile'),
+  (r'^grants/draft-file/(?P<draft_id>\d+)-(?P<file_type>.*)\.', 'ViewDraftFile'),
+  (r'^grants/blocked', 'CannotView'),
+)
+
--- sjfnw/urls.py
+++ sjfnw/urls.py
@@ -1,135 +1,52 @@
 ﻿from django.conf.urls import patterns, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic.simple import direct_to_template
-from admin import advanced_admin
-from sjfnw import constants
+from sjfnw.admin import advanced_admin
 import views
+from grants.urls import apply_urls, grants_urls
 
 handler404 = 'views.page_not_found'
 handler500 = 'views.server_error'
 
+admin.autodiscover() # load admin.py from all apps
+
 urlpatterns = patterns('',
   (r'^/?$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+
+  # project central
+  (r'^fund/?', include('sjfnw.fund.urls')),
+  (r'^fund/logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/fund'}),
+
+  # grants
+  (r'^apply/?', include(apply_urls)),
+  (r'^grants/?', include(grants_urls)),
+  (r'^org/?$', 'sjfnw.grants.views.RedirToApply'),
+  (r'^logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/apply'}),
+  (r'^get-upload-url/(?P<draft_id>\d+)/?$','sjfnw.grants.views.RefreshUploadUrl'), #TODO put this under /apply
 
   # admin
   (r'^admin/', include(admin.site.urls)),
   (r'^admin$', views.admin_redirect),
   (r'^admin-advanced/', include(advanced_admin.urls)),
   (r'^admin-advanced$', views.admin_adv_redirect),
+  (r'^admin/grants/grantapplication/(?P<app_id>\d+)/revert', 'sjfnw.grants.views.AppToDraft'),
+  (r'^admin-advanced/grants/grantapplication/(?P<app_id>\d+)/revert', 'sjfnw.grants.views.AppToDraft'),
+  (r'^admin/grants/grantapplication/(?P<app_id>\d+)/rollover', 'sjfnw.grants.views.AdminRollover'),
+  (r'^admin-advanced/grants/grantapplication/(?P<app_id>\d+)/rollover', 'sjfnw.grants.views.AdminRollover'),
+  (r'^admin/grants/organization/login', 'sjfnw.grants.views.Impersonate'),
+  #reporting
+  (r'^admin/grants/search/?', 'sjfnw.grants.views.SearchApps'),
 
-  # logout
-  (r'^logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/apply'}),
-  (r'^fund/logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/fund'}),
+  # cron emails
+  (r'^mail/overdue-step', 'sjfnw.fund.views.EmailOverdue'),
+  (r'^mail/new-accounts', 'sjfnw.fund.views.NewAccounts'),
+  (r'^mail/gifts', 'sjfnw.fund.views.GiftNotify'),
+  (r'^mail/drafts/?', 'sjfnw.grants.views.DraftWarning'),
 
   # dev
   (r'^dev/jslog/?', 'views.log_javascript'),
   )
 
-# password resets
-urlpatterns += patterns('',
-  # grants
-  (r'^apply/reset/?$', 'django.contrib.auth.views.password_reset', {'template_name':'grants/reset.html', 'from_email':constants.GRANT_EMAIL, 'email_template_name':'grants/password_reset_email.html', 'post_reset_redirect':'/apply/reset-sent'}),
-  (r'^apply/reset-sent/?', 'django.contrib.auth.views.password_reset_done', {'template_name':'grants/password_reset_done.html'}),
-  (r'^apply/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/?$', 'django.contrib.auth.views.password_reset_confirm', {'template_name':'grants/password_reset_confirm.html', 'post_reset_redirect': '/apply/reset-complete'}, 'org-reset'),
-  (r'^apply/reset-complete/?', 'django.contrib.auth.views.password_reset_complete', {'template_name':'grants/password_reset_complete.html'}),
-
-  # fund
-  (r'^fund/reset/?$', 'django.contrib.auth.views.password_reset', {'template_name':'fund/reset.html', 'from_email':constants.FUND_EMAIL, 'email_template_name':'fund/password_reset_email.html', 'subject_template_name':'registration/password_reset_subject.txt'}),
-  (r'^fund/reset-sent/?', 'django.contrib.auth.views.password_reset_done', {'template_name':'fund/password_reset_done.html'}),
-  (r'^fund/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/?$', 'django.contrib.auth.views.password_reset_confirm', {'template_name':'fund/password_reset_confirm.html', 'post_reset_redirect': '/fund/reset-complete'}, 'fund-reset'),
-  (r'^fund/reset-complete/?', 'django.contrib.auth.views.password_reset_complete', {'template_name':'fund/password_reset_complete.html'}),
-  )
-
-# GRANTS
-urlpatterns += patterns('',
-  (r'^apply/nr', direct_to_template, {'template': 'grants/not_grantee.html'}),
-  (r'^apply/submitted/?', direct_to_template, {'template': 'grants/submitted.html'}),
-  )
-urlpatterns += patterns('grants.views',
-
-  (r'^org/?$', 'RedirToApply'),
-
-  #login, logout, registration
-  (r'^apply/login/?$', 'org_login'),
-  (r'^apply/register/?$', 'org_register'),
-
-  #home page
-  (r'^apply/?$','org_home'),
-  (r'^apply/(?P<draft_id>\d+)/DELETE/?$', 'DiscardDraft'),
-  (r'^apply/copy/?$', 'CopyApp'),
-  (r'^apply/support/?', 'org_support'),
-
-  #application
-  (r'^apply/(?P<cycle_id>\d+)/?$','Apply'),
-  (r'^apply/info/(?P<cycle_id>\d+)/?$','cycle_info'),
-
-  #application ajax
-  (r'^apply/(?P<draft_id>\d+)/add-file/?$', 'AddFile'),
-  (r'^get-upload-url/(?P<draft_id>\d+)/?$','RefreshUploadUrl'),
-  (r'^apply/(?P<draft_id>\d+)/remove/(?P<file_field>.*)/?$', 'RemoveFile'),
-  (r'^apply/(?P<cycle_id>\d+)/autosave/?$','AutoSaveApp'),
-
-  #cron
-  (r'^mail/drafts/?', 'DraftWarning'),
-
-  #admin
-  (r'^admin/grants/grantapplication/(?P<app_id>\d+)/revert', 'AppToDraft'),
-  (r'^admin-advanced/grants/grantapplication/(?P<app_id>\d+)/revert', 'AppToDraft'),
-  (r'^admin/grants/grantapplication/(?P<app_id>\d+)/rollover', 'AdminRollover'),
-  (r'^admin-advanced/grants/grantapplication/(?P<app_id>\d+)/rollover', 'AdminRollover'),
-  (r'^admin/grants/organization/login', 'Impersonate'),
-
-  #reading
-  (r'^grants/view/(?P<app_id>\d+)/?$', 'ReadApplication'),
-  (r'^grants/view-file/(?P<app_id>\d+)-(?P<file_type>.*)\.', 'ViewFile'),
-  (r'^grants/draft-file/(?P<draft_id>\d+)-(?P<file_type>.*)\.', 'ViewDraftFile'),
-  (r'^grants/blocked', 'CannotView'),
-
-  #reporting
-  (r'^admin/grants/search/?', 'SearchApps'),
-  )
-
-# PROJECT CENTRAL
-urlpatterns += patterns('fund.views',
-
-  #login, logout, registration
-  (r'^fund/login/?$', 'FundLogin'),
-  (r'^fund/register/?$', 'Register'),
-  (r'^fund/registered/?$', 'Registered'),
-
-  #manage memberships
-  (r'^fund/projects/?', 'Projects'),
-  (r'^fund/set-current/(?P<ship_id>\d+)/?', 'SetCurrent'),
-
-  #main pages
-  (r'^fund/?$', 'Home'),
-  (r'^fund/gp/?', 'ProjectPage'),
-  (r'^fund/grants/?', 'GrantList'),
-
-  #forms - contacts
-  (r'^fund/addmult', 'AddMult'),
-  (r'^fund/(?P<donor_id>\d+)/edit','EditDonor'),
-  (r'^fund/(?P<donor_id>\d+)/delete', 'DeleteDonor'),
-  (r'^fund/add-estimates', 'AddEstimates'),
-
-  #forms - steps
-  (r'^fund/(?P<donor_id>\d+)/step$','AddStep'),
-  (r'^fund/stepmult$','AddMultStep'),
-  (r'^fund/(?P<donor_id>\d+)/(?P<step_id>\d+)$','EditStep'),
-  (r'^fund/(?P<donor_id>\d+)/(?P<step_id>\d+)/done','DoneStep'),
-
-  #error/help pages
-  (r'^fund/not-member/?', 'NotMember'),
-  (r'^fund/pending/?$', 'NotApproved'),
-  (r'^fund/support/?', 'Support'),
-  (r'^fund/blocked/?$', 'Blocked'),
-
-  #cron
-  (r'^mail/overdue-step', 'EmailOverdue'),
-  (r'^mail/new-accounts', 'NewAccounts'),
-  (r'^mail/gifts', 'GiftNotify'),
-  )
 
 #for dev_appserver
 urlpatterns += staticfiles_urlpatterns()
