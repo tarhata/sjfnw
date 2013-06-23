749fcac0b65422462a127db53cbbdff7ae6b7e50
aisa <aisapatino@gmail.com>
2013-06-23 01:44:31 -0700

    copied & modified versions for djangoappengine storage & mail #229

---
+++ sjfnw/grants/storage.py
@@ -0,0 +1,213 @@
+import mimetypes
+import logging
+
+from django.core.files.base import File
+from django.core.files.storage import Storage
+from django.core.files.uploadedfile import UploadedFile
+from django.core.files.uploadhandler import FileUploadHandler, StopFutureHandlers
+from django.utils.encoding import force_unicode
+
+from google.appengine.api import files
+from google.appengine.api.images import get_serving_url, NotImageError
+from google.appengine.ext.blobstore import BlobInfo, BlobKey, delete, BLOB_KEY_HEADER, BLOB_RANGE_HEADER, BlobReader
+
+from sjfnw.grants.utils import FindBlobKey
+
+# MODIFIED VERSION OF DJANGOAPPENGINE'S STORAGE.PY FILE
+# SEE LICENSE AT BOTTOM
+
+class BlobstoreStorage(Storage):
+  """Google App Engine Blobstore storage backend."""
+
+  def _open(self, name, mode='rb'):
+    logging.info('storage open ' + str([name]))
+    return BlobstoreFile(name, mode, self)
+
+  def _save(self, name, content):
+
+    name = name.replace('\\', '/')
+    logging.info('storage _save on ' + str([name]))
+    if hasattr(content, 'file') and hasattr(content.file, 'blobstore_info'):
+      data = content.file.blobstore_info
+      #logging.debug('1st')
+    elif hasattr(content, 'blobstore_info'):
+      data = content.blobstore_info
+      #logging.debug('2nd')
+    elif isinstance(content, File):
+      #logging.debug('3rd, is file')
+      guessed_type = mimetypes.guess_type(name)[0]
+      file_name = files.blobstore.create(mime_type=guessed_type or 'application/octet-stream', _blobinfo_uploaded_filename=name)
+
+      with files.open(file_name, 'a') as f:
+        for chunk in content.chunks():
+          f.write(chunk)
+
+      files.finalize(file_name)
+
+      data = files.blobstore.get_blob_key(file_name)
+
+    else:
+      raise ValueError("The App Engine storage backend only supports "
+                       "BlobstoreFile instances or File instances.")
+
+    if isinstance(data, (BlobInfo, BlobKey)):
+      # We change the file name to the BlobKey's str() value.
+      if isinstance(data, BlobInfo):
+        logging.info('data is blobinfo, storing its key')
+        data = data.key()
+
+      name = name.lstrip('/')
+      if len(name) > 65: #shorten it so extension fits in FileField
+        name = name.split(".")[0][:60].rstrip() + u'.' + name.split(".")[1]
+        logging.info(name)
+        logging.info('Returning ' + str(data) + name )
+        return '%s/%s' % (data, name)
+      else:
+        raise ValueError("The App Engine Blobstore only supports "
+                         "BlobInfo values. Data can't be uploaded "
+                         "directly. You have to use the file upload "
+                         "handler.")
+  def delete(self, name):
+    delete(self._get_key(name))
+
+  def exists(self, name):
+    return self._get_blobinfo(name) is not None
+
+  def size(self, name):
+    return self._get_blobinfo(name).size
+
+  def url(self, name):
+    try:
+      return get_serving_url(self._get_blobinfo(name))
+    except NotImageError:
+      return None
+
+  def created_time(self, name):
+    return self._get_blobinfo(name).creation
+
+  def get_valid_name(self, name):
+    return force_unicode(name).strip().replace('\\', '/')
+
+  def get_available_name(self, name):
+    return name.replace('\\', '/')
+
+  def _get_key(self, name):
+    return BlobKey(name.split('/', 1)[0])
+
+  def _get_blobinfo(self, name):
+    return BlobInfo.get(self._get_key(name))
+
+class BlobstoreFile(File):
+
+  def __init__(self, name, mode, storage):
+    logging.info('.BlobstoreFile__init on ' + name)
+    self.name = name
+    self._storage = storage
+    self._mode = mode
+    self.blobstore_info = storage._get_blobinfo(name)
+
+  @property
+  def size(self):
+    return self.blobstore_info.size
+
+  def write(self, content):
+    raise NotImplementedError()
+
+  @property
+  def file(self):
+    if not hasattr(self, '_file'):
+      self._file = BlobReader(self.blobstore_info.key())
+    return self._file
+
+class BlobstoreFileUploadHandler(FileUploadHandler):
+  """
+  File upload handler for the Google App Engine Blobstore.
+  """
+
+  def new_file(self, *args, **kwargs):
+    """field_name, file_name, content_type, content_length, charset=None"""
+
+    logging.info('BlobstoreFileUploadHandler.new_file')
+    super(BlobstoreFileUploadHandler, self).new_file(*args, **kwargs)
+
+    blobkey = FindBlobKey(self.request.body)
+    self.active = blobkey is not None
+    if self.active:
+      self.blobkey = BlobKey(blobkey)
+      raise StopFutureHandlers()
+
+  def receive_data_chunk(self, raw_data, start):
+    """
+    Add the data to the StringIO file.
+    """
+    if not self.active:
+      return raw_data
+
+  def file_complete(self, file_size):
+    """
+    Return a file object if we're activated.
+    """
+    logging.info('BlobstoreFileUploadHandler.file_complete')
+    if not self.active:
+      logging.info('not active')
+      return
+    return BlobstoreUploadedFile(
+      blobinfo=BlobInfo(self.blobkey),
+      charset=self.charset)
+
+class BlobstoreUploadedFile(UploadedFile):
+  """
+  A file uploaded into memory (i.e. stream-to-memory).
+  """
+
+  def __init__(self, blobinfo, charset):
+    logging.info('BlobstoreUploadedFile.__init__')
+    super(BlobstoreUploadedFile, self).__init__(
+      BlobReader(blobinfo.key()), blobinfo.filename,
+      blobinfo.content_type, blobinfo.size, charset)
+    self.blobstore_info = blobinfo
+
+  def open(self, mode=None):
+    pass
+
+  def chunks(self, chunk_size=1024 * 128):
+    self.file.seek(0)
+    while True:
+      content = self.read(chunk_size)
+      if not content:
+        break
+      yield content
+
+  def multiple_chunks(self, chunk_size=1024 * 128):
+    return True
+
+#Djangoappengine license:
+
+#Copyright (c) Waldemar Kornewald, Thomas Wanschik, and all contributors.
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without modification,
+#are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. Neither the name of All Buttons Pressed nor
+#       the names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
---
+++ sjfnw/mail.py
@@ -0,0 +1,117 @@
+import logging
+from email.mime.base import MIMEBase
+
+from django.conf import settings
+from django.core.mail.backends.base import BaseEmailBackend
+from django.core.mail import EmailMultiAlternatives
+
+from google.appengine.api import mail as gaemail
+from google.appengine.ext import deferred
+from google.appengine.runtime import apiproxy_errors
+
+# MODIFIED VERSION OF DJANGOAPPENGINE'S MAIL.PY FILE
+# SEE LICENSE AT BOTTOM
+
+def _send_deferred(message, fail_silently=False):
+  try:
+    message.send()
+  except (gaemail.Error, apiproxy_errors.Error):
+    if not fail_silently:
+      raise
+
+
+class EmailBackend(BaseEmailBackend):
+  """ Asynchronous email backend """
+
+  can_defer = True
+
+  def send_messages(self, email_messages):
+    """ Call _send for each message & return count of messages """
+
+    num_sent = 0
+    for message in email_messages:
+      if self._send(message):
+        num_sent += 1
+    return num_sent
+
+  def _copy_message(self, message):
+    """ Create and return App Engine EmailMessage class from message """
+
+    gmsg = gaemail.EmailMessage(sender=message.from_email,
+                                to=message.to,
+                                subject=message.subject,
+                                body=message.body)
+    if message.extra_headers.get('Reply-To', None):
+      gmsg.reply_to = message.extra_headers['Reply-To']
+    if message.cc:
+      gmsg.cc = list(message.cc)
+    if message.bcc:
+      gmsg.bcc = list(message.bcc)
+    if message.attachments:
+      # Must be populated with (filename, filecontents) tuples.
+      attachments = []
+      for attachment in message.attachments:
+        if isinstance(attachment, MIMEBase):
+          attachments.append((attachment.get_filename(),
+                             attachment.get_payload(decode=True)))
+        else:
+          attachments.append((attachment[0], attachment[1]))
+      gmsg.attachments = attachments
+    # Look for HTML alternative content.
+    if isinstance(message, EmailMultiAlternatives):
+      for content, mimetype in message.alternatives:
+        if mimetype == 'text/html':
+          gmsg.html = content
+          break
+    return gmsg
+
+  def _send(self, message):
+    """
+    Use _copy_message to convert to gae email obj
+    Call _defer_message to add to send queue
+    """
+    try:
+      message = self._copy_message(message)
+    except (ValueError, gaemail.InvalidEmailError), err:
+      logging.error(err)
+      if not self.fail_silently:
+        raise
+      return False
+    self._defer_message(message)
+    return True
+
+  def _defer_message(self, message):
+    queue_name = getattr(settings, 'EMAIL_QUEUE_NAME', 'default')
+    deferred.defer(_send_deferred, message, fail_silently=self.fail_silently,
+                   _queue=queue_name)
+
+#Djangoappengine license:
+
+#Copyright (c) Waldemar Kornewald, Thomas Wanschik, and all contributors.
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without modification,
+#are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. Neither the name of All Buttons Pressed nor
+#       the names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
