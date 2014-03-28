5e6ff47d4fdb4aa62cde035b1eb299c414660a0a
aisa <aisapatino@gmail.com>
2014-03-28 13:40:08 -0700

    re-added city and state to org. cache for reporting #435

--- sjfnw/grants/models.py
+++ sjfnw/grants/models.py
@@ -112,6 +112,9 @@ class Organization(models.Model):
   phone = models.CharField(max_length=20, blank=True)
   email_address = models.EmailField(max_length=100, blank=True)
 
+  city = models.CharField(max_length=50, blank=True)
+  state = models.CharField(max_length=2, choices=STATE_CHOICES, blank=True)
+
   def __unicode__(self):
     return self.name
 
