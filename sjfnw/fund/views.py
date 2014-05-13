3015a8f1ea7a1c22973faebbdda8a2fbe180a376
Aisa <aisapatino@gmail.com>
2014-05-13 10:20:21 -0700

    fix e4f808b use pre_screening_status #661

--- sjfnw/fund/views.py
+++ sjfnw/fund/views.py
@@ -57,7 +57,7 @@ def get_block_content(membership, get_steps=True):
   p_apps = p_apps.select_related('giving_project', 'application',
       'application__organization')
   # never show screened out by sub-committee
-  p_apps = p_apps.exclude(screening_status = 45)
+  p_apps = p_apps.exclude(pre_screening_status=45)
   if membership.giving_project.site_visits == 1:
     logger.info('Filtering grants for site visits')
     p_apps = p_apps.filter(screening_status__gte=70)
